04248ffa5020ecbea6946e218c8fe7ff678d7b46
Atenajasim <atenamay2020@gmail.com>
2022-08-04 22:49:56 -0400

    8/4/2022

---
+++ suPort/QuizViewController.swift
@@ -0,0 +1,39 @@
+//
+//  QuizViewController.swift
+//  suPort
+//
+//  Created by Atena on 8/4/22.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    
+    @IBOutlet weak var label: UILabel!
+    
+    let arrayOfNames = ["Michèle Mouton" , "Merritt Moore" , "Serena Williams" , "Tai Tzu Ying" , "Sarah Fuller" , "Lisa Leslie" , "Jenny Finch" , "Alyssa Nakken" , "Simone Biles" , "Alex Morgan"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        let randomNumber = Int.random(in: 0...(arrayOfNames.count-1))
+        label.text = arrayOfNames[randomNumber]
+    }
+}
